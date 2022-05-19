7b71ec15c4e42550b287a702c1f2b721b2e8526c
kyocoolcool <kyocoolcool@hotmail.com>
2022-05-19 11:03:17 +0800

    add modal feature

--- furseal-frontend/src/app/main/bill/bill-detail/bill-detail.component.ts
+++ furseal-frontend/src/app/main/bill/bill-detail/bill-detail.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
+import {Component, Input, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {Router} from '@angular/router';
 
 import {Subject} from 'rxjs';
@@ -6,10 +6,11 @@ import {takeUntil} from 'rxjs/operators';
 
 import {CoreSidebarService} from '@core/components/core-sidebar/core-sidebar.service';
 import {repeaterAnimation} from '../bill.animation';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {BillEditService} from '../bill-edit/bill-edit.service';
 import {KeycloakService} from 'keycloak-angular';
 import {environment} from '../../../../environments/environment';
+import {NgbActiveModal, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
     selector: 'app-bill-detail',
@@ -19,9 +20,9 @@ import {environment} from '../../../../environments/environment';
     encapsulation: ViewEncapsulation.None
 })
 export class BillDetailComponent implements OnInit, OnDestroy {
-    public imageUrl:any = environment.imageUrl;
+    public imageUrl: any = environment.imageUrl;
     roles: string[];
-    infinity=Infinity
+    infinity = Infinity
     // Public
     public url = this.router.url;
     public urlLastValue;
@@ -68,7 +69,8 @@ export class BillDetailComponent implements OnInit, OnDestroy {
         private router: Router,
         private _invoiceEditService: BillEditService,
         private _coreSidebarService: CoreSidebarService,
-        private keycloakService: KeycloakService
+        private keycloakService: KeycloakService,
+        private modalService: NgbModal
     ) {
         this._unsubscribeAll = new Subject();
         console.log(`this url ${this.url.lastIndexOf('/') + 1}`);
@@ -125,7 +127,7 @@ export class BillDetailComponent implements OnInit, OnDestroy {
         console.log(this.apiData);
         this.averageSalary = ((this.apiData.money - this.apiData.fee) * (1 - this.apiData.tax / 100)) / this.apiData.members.length;
         this.roles = this.keycloakService.getUserRoles();
-        this.imageUrl += this.urlLastValue+'?random='+ Math.random();
+        this.imageUrl += this.urlLastValue + '?random=' + Math.random();
     }
 
     /**
@@ -140,4 +142,39 @@ export class BillDetailComponent implements OnInit, OnDestroy {
     haveAdmin(): boolean {
         return !(this.roles.indexOf('ADMIN') > -1);
     }
+
+    withAutofocus = `<button type="button" ngbAutofocus class="btn btn-danger"
+      (click)="modal.close('Ok click')">關閉</button>`;
+
+
+    open() {
+        const modalRef = this.modalService.open(NgbdModalContent, { windowClass: 'my-class'});
+        modalRef.componentInstance.name = ' 打寶照片';
+        modalRef.componentInstance.imageUrl = this.imageUrl;
+    }
 }
+
+@Component({
+    selector: 'ngbd-modal-content',
+    template: `
+<!--    <div class="modal-header">-->
+<!--      <h4 class="modal-title">Hi there!</h4>-->
+<!--      <button type="button" class="btn-close" aria-label="Close" (click)="activeModal.dismiss('Cross click')"></button>-->
+<!--    </div>-->
+    <div class="modal-body">
+      <p>{{name}}</p>
+        <img class="card-img-top"  [src]=imageUrl />
+    </div>
+    <div class="modal-footer">
+      <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+    </div>
+  `
+})
+export class NgbdModalContent {
+    @Input() name;
+    @Input() imageUrl;
+
+    constructor(public activeModal: NgbActiveModal) {}
+}
+
+
